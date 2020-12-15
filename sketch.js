0778a6d118d7cd0b5e6abee26964f3cce87673c3
rileyyoung <71328631+rileyyoung@users.noreply.github.com>
2020-12-15 10:11:26 -0500

    website files

---
+++ sketch.js
@@ -0,0 +1,31 @@
+function setup() {
+  createCanvas(innerWidth, innerWidth);
+  strokeWeight(.5);
+  stroke(86, 225,179);
+  frameRate(1); //x frames/sec
+}
+
+function draw() {
+  clear()
+  background(254,216,200)
+  var margin = 30;
+  var width = innerWidth - 2*margin;
+  var height = innerWidth - 2*margin;
+  
+  var shift = random(0,5);
+  var space = random(1,15);
+  
+  var numSquares = 10;
+  var sidelen = width/numSquares;
+  
+  translate (margin,margin);
+  for(var y=0; y < height; y=y+sidelen){
+  for(var x=0; x < width; x=x+sidelen){
+  fill(random(15,300),30,700)
+    
+    quad(x + space + random(-shift, shift),y + space + random(-shift, shift), x+sidelen - space + random(-shift, shift),y + space + random(-shift, shift), x+sidelen - space + random(-shift, shift),y+sidelen - space + random(-shift, shift), x + space + random(-shift, shift),y+sidelen - space + random(-shift, shift));
+  }
+  }
+  
+  // noLoop();
+}
